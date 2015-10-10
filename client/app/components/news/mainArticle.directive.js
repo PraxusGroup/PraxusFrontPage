7f9b2144f5febb84800aca1a91fe2069d23b4a1a
Mathew Kleppin <matkle414@gmail.com>
2015-10-10 02:27:56 -0500

    animation working

--- client/app/components/news/mainArticle.directive.js
+++ client/app/components/news/mainArticle.directive.js
@@ -18,7 +18,33 @@
 
     function link(scope, element, attrs) {
 
-      
+      var content = $(element.find('.card-content'));
+      var image   = $(element.find('.card-image'));
+      var card    = $(element.find('.card'));
+      var options = {
+        duration: 600,
+        queue: false,
+        easing: [0.620, -0.005, 0.260, 0.995]
+      };
+
+      content.on('mouseenter', animateOpen);
+      card.on('mouseleave', animateClose);
+
+      function animateOpen(e) {
+        if (!content.hasClass('velocity-animating')) {
+          console.log('do');
+          content.velocity( { height: '30em' }, options);
+          image.velocity(   { height: '10em' }, options);
+        }
+      }
+
+      function animateClose(e) {
+        if (!content.hasClass('velocity-animating')) {
+          console.log('out');
+          content.velocity( { height: '10em' }, options);
+          image.velocity(   { height: '30em' }, options);
+        }
+      }
 
     }
   }
